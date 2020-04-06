c43fab824595af927091e2050712d8ed32633636
Carlos Estela Blanco <carlos@MacBook-Pro-de-Carlos.local>
2020-04-06 22:18:24 +0200

    index y lenguajes acabados

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 import "./style.css"
 import "@fortawesome/fontawesome-free/css/all.css"
+
+var i = 0;
+var txt = "Lorem ipsum dolor sit amet consectetu";
+var speed = 50;
+
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("intro").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+
+window.onload = typeWriter;

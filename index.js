30f34633493c393d8306e1a653a45396fed4f313
Carlos Estela Blanco <carlos@MacBook-Pro-de-Carlos.local>
2020-04-07 01:03:38 +0200

    categorias añadidas

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-import "./style.css"
 import "@fortawesome/fontawesome-free/css/all.css"
 
 var i = 0;
-var txt = "Lorem ipsum dolor sit amet consectetu";
+var txtLenguajes = "HTML, CSS y JavaScript comforman el grupo de los lenguajes usados en el desarrollo front-end... ¿Sobre qué lenguaje te gustaría saber más?"
+var txtModule = "Los module bundlers son herramientas de desarrollo front-end que permiten empaquetar módulos de JavaScript en un único archivo de JavaScript que ha sido optimizado... ¿Sobre qué module bundler te gustaría saber más?"
 var speed = 50;
 
 
 function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("intro").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-    }
-}
 
+    if (document.getElementById("introLenguajes")) {
+        if (i < txtLenguajes.length) {
+            document.getElementById("introLenguajes").innerHTML += txtLenguajes.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    } else {
+        if (i < txtModule.length) {
+            document.getElementById("introModule").innerHTML += txtModule.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+
+
+}
 
 window.onload = typeWriter;

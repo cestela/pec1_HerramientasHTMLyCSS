2e1d3c15a2b4920566fb26e97f90f5eea8907785
Carlos Estela Blanco <carlos@MacBook-Pro-de-Carlos.local>
2020-04-19 00:23:33 +0200

    estructura finalizada

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,28 +1,28 @@
-import "@fortawesome/fontawesome-free"
+import "@fortawesome/fontawesome-free/js/all.min.js";
 
 var i = 0;
-var txtLenguajes = "HTML, CSS y JavaScript comforman el grupo de los lenguajes usados en el desarrollo front-end... ¿Sobre qué lenguaje te gustaría saber más?"
-var txtModule = "Los module bundlers son herramientas de desarrollo front-end que permiten empaquetar módulos de JavaScript en un único archivo de JavaScript que ha sido optimizado... ¿Sobre qué module bundler te gustaría saber más?"
+var txtLenguajes =
+  "HTML, CSS y JavaScript comforman el grupo de los lenguajes usados en el desarrollo front-end... ¿Sobre qué lenguaje te gustaría saber más?";
+var txtModule =
+  "Los module bundlers son herramientas de desarrollo front-end que permiten empaquetar módulos de JavaScript en un único archivo de JavaScript que ha sido optimizado... ¿Sobre qué module bundler te gustaría saber más?";
 var speed = 50;
 
-
 function typeWriter() {
-
-    if (document.getElementById("introLenguajes")) {
-        if (i < txtLenguajes.length) {
-            document.getElementById("introLenguajes").innerHTML += txtLenguajes.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-        }
-    } else {
-        if (i < txtModule.length) {
-            document.getElementById("introModule").innerHTML += txtModule.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-        }
+  if (document.getElementById("introLenguajes")) {
+    if (i < txtLenguajes.length) {
+      document.getElementById(
+        "introLenguajes"
+      ).innerHTML += txtLenguajes.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
     }
-
-
+  } else {
+    if (i < txtModule.length) {
+      document.getElementById("introModule").innerHTML += txtModule.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
 }
 
 window.onload = typeWriter;
